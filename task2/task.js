a67aed58c1b1956ff946b9d0c218cc656ed11e26
Lilla Klátyik <lemoon885@gmail.com>
2026-03-27 14:32:31 +0100

    finished

--- task2/task.js
+++ task2/task.js
@@ -1,3 +1,13 @@
 export function findUniqueGenres(genres) {
-
+    const result = [];
+    
+    for (const movieGenres of genres) {
+        for (const genre of movieGenres) {
+            if (!result.includes(genre)) {
+                result.push(genre);
+            }
+        }
+    }
+    
+    return result;
 }
